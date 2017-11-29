c51ef23d76cb11524c0bdd0f87c6fd45302116d4
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-11-28 15:04:51 -1000

    initial commit

--- app/lib/url.js
+++
@@ -1,4 +0,0 @@
-//note: if you're running on your own computer, change this to http://localhost:8080/api/
-//if you're not, change it to like, http://10.0.1.8:8080/api/
-
-export const url = 'http://localhost:8080/api/';
