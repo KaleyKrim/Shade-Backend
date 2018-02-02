01f46666d95ab4edf87369825730ab3a831714e9
Kaley Kriminger <kaley.kriminger@gmail.com>
2018-02-01 23:33:10 -0800

    add associations to flag_one thru three

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,6 @@ module.exports = function (sequelize, DataTypes) {
     password: {type: DataTypes.STRING, allowNull: false},
     email: {type: DataTypes.STRING, allowNull: false},
     points: {type: DataTypes.INTEGER, defaultValue: 0},
-    emoji_id: {type: DataTypes.INTEGER, allowNull: false},
     status_id: {type: DataTypes.INTEGER, defaultValue: 1},
     deletedAt: {type: DataTypes.DATEONLY, defaultValue: null},
     role_id: {type: DataTypes.INTEGER, defaultValue: 1}
@@ -13,10 +12,6 @@ module.exports = function (sequelize, DataTypes) {
   });
 
   User.associate = function (models) {
-    User.belongsTo(models.emoji, {
-      foreignKey: 'emoji_id',
-      as: 'icon'
-    });
     User.belongsTo(models.status, {
       foreignKey: 'status_id',
       as: 'user_status'
@@ -24,6 +19,18 @@ module.exports = function (sequelize, DataTypes) {
     User.hasMany(models.message, {
       foreignKey: 'shader_id',
       as: 'offense'
+    });
+    User.hasMany(models.message, {
+      foreignKey: 'flag_one',
+      as: 'offended_user'
+    });
+    User.hasMany(models.message, {
+      foreignKey: 'flag_two',
+      as: 'another_offended_user'
+    });
+    User.hasMany(models.message, {
+      foreignKey: 'flag_three',
+      as: 'yet_another_offended_user'
     });
     User.hasMany(models.message, {
       foreignKey: 'victim_id',
