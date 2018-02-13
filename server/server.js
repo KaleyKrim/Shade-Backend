8720915c11733680fcfae0acb1c9029a2d053ffc
Kaley Kriminger <kaley.kriminger@gmail.com>
2018-02-12 18:40:21 -0800

    add middleware to allow CORS

--- server/server.js
+++ server/server.js
@@ -26,6 +26,13 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use('/api', routes);
 
 app.listen(PORT, () => {
