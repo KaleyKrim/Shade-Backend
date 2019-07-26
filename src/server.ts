d6a7262553700ef295947a8387ea372f855aee66
Kaley Kriminger <kaley.kriminger@gmail.com>
2019-07-27 00:39:40 +0900

    chain middlewares

--- src/server.ts
+++ src/server.ts
@@ -4,30 +4,22 @@ import * as session from "express-session";
 import * as passport from "passport";
 import * as bodyParser from "body-parser";
 import * as connectRedis from "connect-redis";
-import * as path from "path";
-import * as db from "./models";
+// import * as db from "./models";
 import * as routes from "./routes";
 import * as middleware from "./middlewares";
 const RedisStore = connectRedis(session);
-const PORT = require(`../config/${process.env.NODE_ENV}`).PORT;
 
-const app = express();
-app.use(compression());
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use(bodyParser.urlencoded({ "extended" : false }));
-app.use(bodyParser.json());
-app.use(session({
-	store: new RedisStore({ host: '127.0.0.1', port: 6379 }),
-	secret: 'Shade',
-	resave: false,
-	saveUninitialized: false,
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(middleware.headerSetter);
-
-app.use('/api', routes);
-
-module.exports = app;
+export const app = express()
+	.use(compression())
+	.use(bodyParser.urlencoded({ "extended" : false }))
+	.use(bodyParser.json())
+	.use(session({
+		store: new RedisStore({ host: '127.0.0.1', port: 6379 }),
+		secret: 'Shade',
+		resave: false,
+		saveUninitialized: false,
+	}))
+	.use(passport.initialize())
+	.use(passport.session())
+	.use(middleware.headerSetter)
+	.use('/api', routes);
